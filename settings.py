25a29c3606d87f140e5e3f0115d1422a569d4e76
MSMikl <michalbl4@gmail.com>
2022-06-06 14:22:12 +0300

    docker added

--- settings.py
+++ settings.py
@@ -1,11 +1,21 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv
+
 INSTALLED_APPS = [
     'devman.apps.DevmanConfig',
 ]
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'db.sqlite3',
+        'ENGINE': os.getenv('DB_ENGINE'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASS'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT')
     }
 }
 
